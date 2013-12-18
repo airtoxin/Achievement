685c23a3b4208a1547d9a98b3739a39937c79cec
airtoxin <tahirimaka6@gmail.com>
2013-12-19 07:09:34 +0900

    解除した実績の一覧が新しいウィンドウで表示されるように変更

--- achievement.py
+++ achievement.py
@@ -1,10 +1,40 @@
-import sublime, sublime_plugin
+import sublime
+import sublime_plugin
+import json
 
 class AutoRunner(sublime_plugin.EventListener):
     def on_post_save(self, view):
+        self._count_save()
+
+    def _count_save(self):
         setting = sublime.load_settings("achievement.sublime-settings")
-        num_save = setting.get("num_save")
-        sublime.message_dialog(str(num_save))
-        num_save += 1
-        setting.set("num_save", num_save)
+        save_count = setting.get("save_count")
+        save_count += 1
+        self._check_save_achievement(save_count)
+        setting.set("save_count", save_count)
         sublime.save_settings("achievement.sublime-settings")
+
+    def _check_save_achievement(self, save_count):
+        print(save_count)
+        if save_count in range(1000): #(1, 10, 100, 300, 500, 1000):
+            message = "save " + str(save_count) + " times!"
+            sublime.message_dialog(message)
+            unlocked_achievement = sublime.load_settings("unlocked.sublime-settings")
+            unlocked = unlocked_achievement.get("unlocked")
+            unlocked.append(message)
+            unlocked_achievement.set("unlocked", unlocked)
+            sublime.save_settings("unlocked.sublime-settings")
+
+
+class ViewAchievementCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        w = self.view.window()
+        achievement_window = w.new_file()
+        # write unlocked achievement
+        unlocked = sublime.load_settings("unlocked.sublime-settings").get("unlocked")
+        for lock in reversed(unlocked):
+            line = "*\t" + lock + "\n"
+            achievement_window.insert(edit, 0, line)
+
+        message = "*=*=*=*=*=*=*=* UNLOCKED ACHIEVEMENTS *=*=*=*=*=*=*=*\n"
+        achievement_window.insert(edit, 0, message)
