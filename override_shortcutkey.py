b281625d73e3114e598f006a39fc539cbeed6b9f
airtoxin <tahirimaka6@gmail.com>
2013-12-20 12:23:11 +0900

    add achievement paste character size

--- override_shortcutkey.py
+++ override_shortcutkey.py
@@ -14,18 +14,23 @@ def achievement_function(setting_name, message):
     setting_name(str): 'hogehoge'
     message(str): '!!CONGRATULATION!!'
     """
-    achievement_dialog(message)
+    if len(message) == 0:
+        return
     unlocked_settings = sublime.load_settings("unlocked.sublime-settings")
     unlocked_titles = unlocked_settings.get("unlocked_titles", [])
     if setting_name in unlocked_titles:
         c_setting = unlocked_settings.get(setting_name, [])
-        c_setting.append(message)
-        unlocked_settings.set(setting_name, c_setting)
+        if message in c_setting:
+            return
+        else:
+            c_setting.append(message)
+            unlocked_settings.set(setting_name, c_setting)
     else:
         unlocked_titles.append(setting_name)
         unlocked_settings.set("unlocked_titles", unlocked_titles)
         unlocked_settings.set(setting_name, [message])
     sublime.save_settings("unlocked.sublime-settings")
+    achievement_dialog(message)
 
 def count_achievement_function(setting_name, count, achieving_counts, message):
     u"""
@@ -79,11 +84,15 @@ class OverridePasteCommand(sublime_plugin.TextCommand):
     u""" ⌘+v """
     def run(self, edit):
         self.view.run_command("paste")
-        thread = threading.Thread(target=self._command_thread)
-        thread.setDaemon(True)
-        thread.start()
+        paste_command_thread = threading.Thread(target=self._override_paste_command_thread)
+        paste_command_thread.setDaemon(True)
+        paste_command_thread.start()
 
-    def _command_thread(self):
+        count_paste_size_thread = threading.Thread(target=self._count_paste_size_thread)
+        count_paste_size_thread.setDaemon(True)
+        count_paste_size_thread.start()
+
+    def _override_paste_command_thread(self):
         setting = sublime.load_settings("achievement.sublime-settings")
         count = setting.get("paste_count", 0) + 1
         count_achievement_function("paste_count", count, (1,), "Paste")
@@ -91,3 +100,22 @@ class OverridePasteCommand(sublime_plugin.TextCommand):
         count_achievement_function("paste_count", count, (99999999,), "Painter")
         setting.set("paste_count", count)
         sublime.save_settings("achievement.sublime-settings")
+
+    def _count_paste_size_thread(self):
+        setting_name = "pasting"
+        message = ""
+        paste_size = len(sublime.get_clipboard(1073741824)) # if character size over 1073741824(1GB), return 0
+        print(paste_size)
+        if paste_size == 1:
+            message = "ant"
+        elif paste_size == 0:
+            message = "TOO BIG TO PASTE"
+        elif paste_size == 1000000000:
+            message = "JUST A BILLION CHARACTERS PASTE ONE TIME"
+        elif paste_size == 1000000:
+            message = "JUST A MILLION CHARACTERS PASTE ONE TIME"
+        elif paste_size == 1024:
+            message = "JUST A 1024 CHARACTERS PASTE ONE TIME"
+        elif paste_size == 65536:
+            message = "JUST A 2^(2^(2^2)) CHARACTERS PASTE ONE TIME"
+        achievement_function(setting_name, message)
